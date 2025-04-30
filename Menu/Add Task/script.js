a957a6b72f8db3c8332419d8b4a5f14dc327aa27
danielprogram08 <danieldanielsilva08@gmail.com>
2025-04-29 22:07:30 -0300

    Add Tarefa completo!

--- Menu/Add Task/script.js
+++ Menu/Add Task/script.js
@@ -1,3 +1,26 @@
 function Return() {
     window.location.href = '../index.html';
 }
+
+function AddTask() {
+    let title = document.getElementById("title").value;
+    let term = document.getElementById("datetime").value;
+    let description = document.getElementById("description").value;
+
+    const task = {
+        Title: title,
+        Term: term,
+        Description: description
+    };
+
+    json = JSON.stringify(task);
+
+    localStorage.setItem("Task", json);
+    clear();
+}
+
+function clear() {
+    document.getElementById("title").value = '';
+    document.getElementById("datetime").value = '';
+    document.getElementById("description").value = '';
+}
