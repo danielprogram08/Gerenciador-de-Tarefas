b28ce9f7643a932385640055280713f4b7b3491b
danielprogram08 <danieldanielsilva08@gmail.com>
2025-05-24 00:17:13 -0300

    Estilo dos botões Editar e Excluir tarefas concluído!

--- Menu/View Task/script.js
+++ Menu/View Task/script.js
@@ -7,17 +7,19 @@ function ViewTask() {
 
         task.className = "task";
         task.innerHTML = `
-        
+        <div class="titles">
             <h1>${Task.Title}</h1>
             <h4>${Task.Term}</h4>
             <h4>${Task.Description}</h4>
+        </div>
+        <div class="buttons">
             <button id="edit-button">
-                <img src="../../icons/edit_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
+                <img src="../../Icons/edit_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
             </button>
             <button id="delete-button">
-                <img src="../../icons/delete_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
+                <img src="../../Icons/delete_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
             </button>
-        `;
+        </div>`;
 
         document.body.appendChild(task);
     } else {
