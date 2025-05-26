11a1a5d0f7dc461ec26efca92e22adec4d9057db
danielprogram08 <danieldanielsilva08@gmail.com>
2025-05-26 14:31:28 -0300

    Botão Editar da página Visualização concluído!

--- Menu/View Task/script.js
+++ Menu/View Task/script.js
@@ -1,23 +1,28 @@
 function ViewTask() {
+
     const StringTask = localStorage.getItem("Task");
 
     if (StringTask) {
+
         const Task = JSON.parse(StringTask);
-        const DateHour = Task.Term;
-        const parts = DateHour.split('T');
+        const datehour = Task.Term;
+        const parts = datehour.split('T');
         const date = parts[0];
         const hour = parts[1];
+        const DateHour = new Date(date);
+        const DateFormated = DateHour.toLocaleDateString('pt-br');
+        
         let task = document.createElement("div");
 
         task.className = "task";
         task.innerHTML = `
         <div class="titles">
             <h1>${Task.Title}</h1>
-            <h4>${"Data: " + date + "<br>Hora: " + hour}</h4>
+            <h4>${"Data: " + DateFormated + "<br>Hora: " + hour}</h4>
             <h4>${Task.Description}</h4>
         </div>
         <div class="buttons">
-            <button id="edit-button">
+            <button id="edit-button" onclick="EditTask()">
                 <img src="../../Icons/edit_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
             </button>
             <button id="delete-button">
@@ -26,9 +31,15 @@ function ViewTask() {
         </div>`;
 
         document.body.appendChild(task);
+        
     } else {
         console.error("Erro ao tentar acessar a tarefa.");
     }
 }
-
 ViewTask();
+
+function EditTask() {
+    window.location.href = './Edit Task/index.html';
+}
+
+window.EditTask = EditTask;
