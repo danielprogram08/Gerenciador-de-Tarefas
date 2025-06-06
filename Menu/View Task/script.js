f8f0321a8247978bcfc8d3c4cf612d5dd20fd379
danielprogram08 <danieldanielsilva08@gmail.com>
2025-06-06 14:19:51 -0300

    Visualizar Tarefas 50% concluído!

--- Menu/View Task/script.js
+++ Menu/View Task/script.js
@@ -2,31 +2,48 @@ function ViewTask() {
     let tasklist = localStorage.getItem("Key-Application");
     let Tasks = JSON.parse(tasklist);
 
-    Tasks.forEach(task => {
+    if (tasklist) {
+        Tasks.forEach(task => {
         const Tasks = document.createElement("div");
 
         Tasks.className = "tasks";
         Tasks.innerHTML = `
             <div class="titles">
-                <h1>${task.Title}</h1>
+                <h1 id="title">${task.Title}</h1>
+                <p id="term">${task.Term}</p>
+                <p id="description">${task.Description}</p>
             </div>
             <div class="buttons">
-            
+                <div id="edit-button">
+                    <img id="edit-img" src="../../Icons/edit_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
+                </div>
+                <div id="delete-button">
+                    <img id="delete-img" src="../../Icons/delete_16dp_E3E3E3_FILL0_wght400_GRAD0_opsz20.png">
+                </div>
             </div>
         `;
     document.body.appendChild(Tasks);
     });
+
+    const ButtonReturn = document.createElement("div");
+
+        ButtonReturn.className = "Btn-return";
+        ButtonReturn.innerHTML = `<button id="btn-return" onclick="Return()">↩︎</button>`;
+
+    document.body.appendChild(ButtonReturn);
+
+    } else {
+
+        const ButtonReturn = document.createElement("div");
+
+            ButtonReturn.className = "Btn-return";
+            ButtonReturn.innerHTML = `<button id="btn-return" onclick="Return()">↩︎</button>`;
+
+        document.body.appendChild(ButtonReturn);
+    }
 }
 
 ViewTask();
-
-const ButtonReturn = document.createElement("div");
-
-    ButtonReturn.className = "Btn-return";
-    ButtonReturn.innerHTML = `<button id="btn-return" onclick="Return()">↩︎</button>`;
-
-document.body.appendChild(ButtonReturn);
-
 
 function EditTask() {
     window.location.href = './Edit Task/index.html';
