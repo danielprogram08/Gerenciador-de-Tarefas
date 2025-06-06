3cc66368d8f67497ee8468d6c3f051085fab8bfe
danielprogram08 <danieldanielsilva08@gmail.com>
2025-06-06 08:33:36 -0300

    Visualizar Tarefas em andamento!

--- Menu/View Task/script.js
+++ Menu/View Task/script.js
@@ -1,10 +1,32 @@
+function ViewTask() {
+    let tasklist = localStorage.getItem("Key-Application");
+    let Tasks = JSON.parse(tasklist);
+
+    Tasks.forEach(task => {
+        const Tasks = document.createElement("div");
+
+        Tasks.className = "tasks";
+        Tasks.innerHTML = `
+            <div class="titles">
+                <h1>${task.Title}</h1>
+            </div>
+            <div class="buttons">
+            
+            </div>
+        `;
+    document.body.appendChild(Tasks);
+    });
+}
+
+ViewTask();
+
 const ButtonReturn = document.createElement("div");
 
     ButtonReturn.className = "Btn-return";
-    ButtonReturn.innerHTML = `
-        <button id="btn-return" onclick="Return()">↩︎</button>`;
+    ButtonReturn.innerHTML = `<button id="btn-return" onclick="Return()">↩︎</button>`;
 
 document.body.appendChild(ButtonReturn);
+
 
 function EditTask() {
     window.location.href = './Edit Task/index.html';
